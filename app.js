3e7ec7ce448036739b213a7c6c26ddcbe9c5a0ef
krchome <krchome@hotmail.com>
2022-04-03 08:55:38 +1200

    App changed

--- app.js
+++ app.js
@@ -2,12 +2,16 @@
 
 const express = require('express');
 const connectDB = require('./config/db');
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const app = express();
+
 var cors = require('cors');
 
 // routes
 const books = require('./routes/api/books');
 
-const app = express();
+//const app = express();
 
 // Connect Database
 connectDB();
@@ -18,11 +22,46 @@ app.use(cors({ origin: true, credentials: true }));
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('Hello world!'));
-
 // use Routes
 app.use('/api/books', books);
 
-const port = process.env.PORT || 8085;
+//const port = process.env.PORT || 8085;
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+require("./models/Book");
+
+mongoose
+  .connect(
+    "mongodb+srv://krchome1:ddq6645k@cluster0.08all.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("MongoDB has been connected"))
+  .catch((err) => console.log(err));
+  // Init Middleware
+//middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//import routes
+require("./routes/api/books")(app);
+
+// use Routes
+app.use('/api/books', books);
+const PORT = process.env.PORT || 5000;
+
+// Accessing the path module
+const path = require("path");
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./MERN_A_to_Z_Client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./MERN_A_to_Z_Client/build", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
+
