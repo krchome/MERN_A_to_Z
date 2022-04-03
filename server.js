f932b234576f480c4e39791b873bb0cc415fce19
krchome <krchome@hotmail.com>
2022-04-03 19:46:17 +1200

    Changed for heroku

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ var cors = require('cors');
 
 // routes
 const books = require('./routes/api/books');
+require("dotenv").config({path:"./config.env"});
 
 //const app = express();
 
@@ -25,11 +26,11 @@ app.use(express.json({ extended: false }));
 // use Routes
 app.use('/api/books', books);
 
-//const port = process.env.PORT || 8085;
+const port = process.env.PORT;
 
-require("./models/Book");
+//require("./models/Book");
 
-mongoose
+/* mongoose
   .connect(
     "mongodb+srv://krchome1:ddq6645k@cluster0.08all.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     {
@@ -38,7 +39,7 @@ mongoose
     }
   )
   .then(() => console.log("MongoDB has been connected"))
-  .catch((err) => console.log(err));
+  .catch((err) => console.log(err)); */
   // Init Middleware
 //middleware
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -49,17 +50,26 @@ require("./routes/api/books")(app);
 
 // use Routes
 app.use('/api/books', books);
+//if(process.env.NODE_ENV === "production")
+//{
+    app.use(express.static(path.resolve(__dirname,'./MERN_A_to_Z_Client/build')));
+    app.get("*", function (request, response) {
+        response.sendFile(path.resolve(__dirname, "./MERN_A_to_Z_Client/build","index.html"));
+      });
+/* }else{
+   app.get("/",(req, res)=>{
+      res.send("API running");
+   });  */
+//}
 const PORT = process.env.PORT || 5000;
 
 // Accessing the path module
 const path = require("path");
 
 // Step 1:
-app.use(express.static(path.resolve(__dirname, "./MERN_A_to_Z_Client/build")));
+//app.use(express.static(path.resolve(__dirname, "./MERN_A_to_Z_Client/build")));
 // Step 2:
-app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./MERN_A_to_Z_Client/build", "index.html"));
-});
+
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
